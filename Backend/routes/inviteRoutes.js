7ecea2e83383f3eef8c2474e6f1a85acd1dd8f28
Aryan Manu <aryanmanu544@gmail.com>
2025-05-21 11:16:51 +0530

    error fix

--- Backend/routes/inviteRoutes.js
+++ Backend/routes/inviteRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const inviteController = require('../controllers/inviteController');
+const auth = require('../middleware/auth'); 
 
 router.post('/create_invite_link', inviteController.createInvite);
 router.post('/invite_link_info', inviteController.getInviteInfo);
