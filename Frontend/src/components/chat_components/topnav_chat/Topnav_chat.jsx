6e34bec7a3dc152857d342c01a7f1cb4ee9c46cb
Aryan Manu <aryanmanu544@gmail.com>
2025-05-22 21:29:37 +0530

    fixed logout button in server topnav

--- Frontend/src/components/chat_components/topnav_chat/Topnav_chat.jsx
+++ Frontend/src/components/chat_components/topnav_chat/Topnav_chat.jsx
@@ -1,64 +1,66 @@
-import React from 'react'
-import topnav_chatcss from '../topnav_chat/topnav_chat.module.css'
+import React from 'react';
+import topnav_chatcss from '../topnav_chat/topnav_chat.module.css';
 import TagIcon from '@mui/icons-material/Tag';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import PushPinIcon from '@mui/icons-material/PushPin';
 import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
 import InboxIcon from '@mui/icons-material/Inbox';
 import HelpIcon from '@mui/icons-material/Help';
+import LogoutIcon from '@mui/icons-material/Logout';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import Tooltip from 'react-bootstrap/Tooltip';
 import { useSelector } from 'react-redux';
-import LogoutIcon from '@mui/icons-material/Logout';
-
+import { useNavigate } from 'react-router-dom';
 
 function Topnav_chat() {
+  const channel_name = useSelector(state => state.current_page.page_name);
+  const navigate = useNavigate();
 
-  const channel_name = useSelector(state=>state.current_page.page_name)
+  // Tooltip content generator
+  const renderTooltip = (value) => (
+    <Tooltip id={`tooltip-${value}`}>
+      {value}
+    </Tooltip>
+  );
 
-    function buttons(message,Icon){
-        return(
-          <div className={topnav_chatcss.right_comps} onClick={()=>{
-            if(message=='Logout'){
-              localStorage.clear();     
-              window.location.reload();
-            }
-          }}>
-          <OverlayTrigger
-            
-            placement="bottom"
-            overlay={tooltips(message)}>
-            {<Icon/>}
-          </OverlayTrigger>
-        </div>
-        )
-    }
-    
-      const tooltips = (value,props) => (
-        <Tooltip id="button-tooltip" {...props}>
-          {value}
-        </Tooltip>
-      );
+  // Button renderer
+  function buttons(message, Icon) {
+    return (
+      <div
+        className={topnav_chatcss.right_comps}
+        onClick={() => {
+          if (message === 'Logout') {
+            localStorage.clear();
+            navigate('/login');
+          }
+        }}
+      >
+        <OverlayTrigger placement="bottom" overlay={renderTooltip(message)}>
+          <div><Icon /></div>
+        </OverlayTrigger>
+      </div>
+    );
+  }
+
   return (
-    <>
-        <div className={topnav_chatcss.main_wrap}>
-            <div id={topnav_chatcss.left}>
-                <TagIcon></TagIcon>
-                <div id={topnav_chatcss.channel_name}>
-                    {channel_name}
-                </div>
-            </div>
-            <div id={topnav_chatcss.right}>
-                {buttons('Notification Settings' , NotificationsIcon)}
-                {buttons('Pinned Messages' , PushPinIcon)}
-                {buttons('Hide Member List' , PeopleAltIcon)}
-                <input placeholder='Search' type="text" />
-                {buttons('Inbox' , InboxIcon)}
-                {buttons('Logout' , LogoutIcon)}
-            </div>
+    <div className={topnav_chatcss.main_wrap}>
+      <div id={topnav_chatcss.left}>
+        <TagIcon />
+        <div id={topnav_chatcss.channel_name}>
+          {channel_name}
         </div>
-    </>
-  )
+      </div>
+
+      <div id={topnav_chatcss.right}>
+        {buttons('Notification Settings', NotificationsIcon)}
+        {buttons('Pinned Messages', PushPinIcon)}
+        {buttons('Hide Member List', PeopleAltIcon)}
+        <input placeholder="Search" type="text" />
+        {buttons('Inbox', InboxIcon)}
+        {buttons('Logout', LogoutIcon)}
+      </div>
+    </div>
+  );
 }
 
-export default Topnav_chat
+export default Topnav_chat;
