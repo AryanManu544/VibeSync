e581c124e4c13d57de867fe88af44e1047287562
Aryan Manu <aryanmanu544@gmail.com>
2025-05-26 15:02:31 +0530

    fixed post to get

--- Backend/routes/roleRoutes.js
+++ Backend/routes/roleRoutes.js
@@ -5,7 +5,7 @@ const roleController = require('../controllers/roleController');
 router.post('/add_role', roleController.addRole);
 router.post('/assign_role', roleController.assignRole);
 router.post('/remove_role', roleController.removeRole);
-router.post('/get_roles', roleController.getRoles);
+router.get('/get_roles', roleController.getRoles);
 router.post('/get_user_roles', roleController.getUserRoles);
 
 module.exports = router;
