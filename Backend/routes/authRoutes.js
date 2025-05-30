4cf87c00a81ab53e15a650e384bca6514fd139b1
Aryan Manu <aryanmanu544@gmail.com>
2025-05-30 19:02:08 +0530

    bug fix

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express           = require('express');
 const router            = express.Router();
 const authController    = require('../controllers/authController');
-const authMiddleware    = require('../middleware/authToken');
+const authMiddleware    = require('../middleware/auth');
 const cookieParser      = require('cookie-parser');
 
 router.use(cookieParser());
