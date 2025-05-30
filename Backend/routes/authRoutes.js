a8a8b22a93c2bb77d2d52e88463c40ad61c2e45f
Aryan Manu <aryanmanu544@gmail.com>
2025-05-30 19:08:10 +0530

    bug fix

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -3,10 +3,15 @@ const router            = express.Router();
 const authController    = require('../controllers/authController');
 const authMiddleware    = require('../middleware/auth');
 const cookieParser      = require('cookie-parser');
+const upload = require('../controllers/upload'); 
 
 router.use(cookieParser());
 
-router.post('/register', authController.register);
+router.post(
+   '/register',
+   upload.single('profile_pic'),                    
+   authController.register
+ );
 router.post('/login',    authController.login);
 router.post('/logout',   authMiddleware, authController.logout);
 router.post(
